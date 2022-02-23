6864a08a91a6a64b8a597063934674dc87d8d0a2
Nuri Korganci <nkorganci@gmail.com>
2022-02-22 23:24:35 -0600

    Id

--- src/main/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentController.java
@@ -4,10 +4,7 @@ import com.dailycodebuffer.Springboottutorial.entity.Department;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentService;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,11 @@ public class DepartmentController {
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
         return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+            return departmentService.fetchDepartmentById(departmentId);
     }
 
 }
